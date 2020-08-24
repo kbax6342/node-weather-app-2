6b697118df447395c71ce8dfdd52a1ad769639c1
kbax6342 <k3vin.baxt3r@gmail.com>
2020-08-23 20:38:12 -0400

    second http request working

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=5fe95a05677af86821ad8b665e6e7735&query=37.8267,-122.4233'
 
+
+
+const searchText = toString(console.log('What adress are your looking for'))
+
+const forward2Geocoding = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoia2JheDYzNDIiLCJhIjoiY2tlNGdkaGM0MHN6dTJ5bnhxd245bjBjNyJ9.Hyj6dy2Obykg5BS9OyhfZQ&limit=1"
+const url = 'http://api.weatherstack.com/current?access_key=5fe95a05677af86821ad8b665e6e7735&query=37.8267,-122.4233'
 request({
     url: url,
     json: true
 }, (error, response) => {
-    const start = response.body
-    console.log(
 
-        "It is currently " +
+    const start = response.body
+
+    console.log(
+        start.current.weather_descriptions + "! in " + start.location.name + "," +
+        start.location.region +
+        ". It is currently " +
         start.current.temperature +
         " degreess out. There is a " +
         start.current.precip +
@@ -18,6 +26,17 @@ request({
 
 })
 
-//Goal: Print a small forecaset to the user
+//Geocoding
+//Address
 
-//1. Print: "It is currently 58.55 degress out. There is a 0% chance of rain."
+request({
+    url: forward2Geocoding,
+    json: true
+}, (error, response) => {
+    const latitude = response.body.features[1].center[1]
+    const longitude = response.body.features[0].center[0]
+    console.log(
+        "Longitutde:" + longitude + "\n" +
+        "Latitude:" + latitude
+    )
+})
